113c8857335ef279a19562a864c04f921541bc85
Jimmy <JimmyLiu8351@outlook.com>
2019-08-09 14:29:05 +0800

    default settings now work, untested

--- updateModels.js
+++ updateModels.js
@@ -7,12 +7,15 @@ const commander = require('commander');
 const defaultProperties = ['type', 'required', 'length', 'id'];
 
 function readFile(fileName) {
-  fs.readFile('common/models/' + fileName + '.json', (err, buffer) => {
-    if (err) {
-      console.log(fileName + ' does not exist, use -a option to add new models');
-      return;
-    }
-    return JSON.parse(buffer);
+  return new Promise((resolve, reject) => {
+    fs.readFile('common/models/' + fileName + '.json', 'utf8', (err, buffer) => {
+      if (err) {
+        console.log(fileName + '.json does not exist, use -a option to add new models');
+        reject();
+      }
+      console.log(fileName + '.json read successfully');
+      resolve(JSON.parse(buffer));
+    });
   });
 }
 
@@ -26,24 +29,31 @@ function defaultReplace(originalModel, databaseName) {
   else
     tableName = camelToUnderscore(originalModel.name);
 
-  datasources[databaseName].discoverSchema(tableName, function (err, schema) {
-    if (err) {
-      console.log(tableName + ' does not exist in database ' + databaseName);
-      return;
-    }
+  console.log('database table name found: ' + tableName);
+
+  return new Promise((resolve, reject) => {
+    datasources[databaseName].discoverSchema(tableName, function (err, schema) {
+      if (err) {
+        console.log(tableName + ' does not exist in database ' + databaseName);
+        reject();
+      }
 
     // Go through properties in schema
-    for (const property of Object.keys(schema.properties)) {
-      for (const setting of Object.keys(schema.properties[property])) {
-        if (defaultProperties.includes(setting)) {
-          originalModel.properties[property][setting] = schema.properties[property][setting];
-          console.log('Updated model: ' + originalModel.name + ' property: ' + originalModel.properties[property]);
+      for (const property of Object.keys(schema.properties)) {
+        // create properties that don't exist in model
+        if (!originalModel.properties[property]) {
+          originalModel.properties[property] = {};
+        }
+        for (const setting of Object.keys(schema.properties[property])) {
+          if (defaultProperties.includes(setting)) {
+            originalModel.properties[property][setting] = schema.properties[property][setting];
+            console.log('Updated model: ' + originalModel.name + ', Property: ' + property + ', Setting: ' + setting);
+          }
         }
       }
-    }
+      resolve(originalModel);
+    });
   });
-
-  return originalModel;
 }
 
 function camelToHyphen(modelName) {
@@ -60,26 +70,29 @@ function writeFile(newModel) {
   console.log('Model JSON file update complete');
 }
 
+async function commanderHandler(databaseName, modelNames) {
+  // no options selected
+  for (const modelName of modelNames) {
+    const originalModel = await readFile(camelToHyphen(modelName));
+    if (originalModel) {
+      const newModel = await defaultReplace(originalModel, databaseName);
+      if (newModel) {
+        writeFile(newModel);
+      }
+    }
+  }
+}
+
 commander
   .description('An npm module to automatically update Loopback models according to schema')
   .arguments('<databaseName> [modelNames...]')
   .option('-u, --update', 'update all models')
   .option('-a, --add', 'adds or updates stated models from database')
   .option('-e, --addEveryModel', 'add all models from the database')
-  .option('-p, --properties', 'update and add all properties')
+  .option('-p, --properties', 'update and add all non-default properties')
   .option('-d, --delete', 'delete models and properties that are not in database')
   .action((databaseName, modelNames) => {
-    // no options selected
-    for (const modelName of modelNames) {
-      const originalModel = readFile(camelToHyphen(modelName));
-      if (originalModel) {
-        const newModel = defaultReplace(originalModel, databaseName);
-        if (newModel) {
-          writeFile(newModel);
-        }
-      }
-      process.exit(0);
-    }
+    commanderHandler(databaseName, modelNames);
   })
   .parse(process.argv);
 
